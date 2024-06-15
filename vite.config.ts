bb7d6a1e18aeb2e075deccb5ed554a252e85014d
xiaqingsong <927538064@qq.com>
2024-06-16 00:52:54 +0800

    9

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const url = import .meta.url
+
+const repo = url.replace(/.*?\//, "");
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/video/',
+  base: `/${repo}/`,
   plugins: [react()],
 })
